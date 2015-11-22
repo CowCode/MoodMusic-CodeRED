ddbd48230c41eab7e7a402d9424b930e1cd7d98f
j-yang <joshua.yang16@gmail.com>
2015-11-22 10:43:19 -0600

    Getting ready for implementation

--- Sentiment Analysis/test.py
+++ Sentiment Analysis/test.py
@@ -1,5 +1,8 @@
 import indicoio
 import random
+
+#song lists created using radio billboard list and indicoio algorithm on song lyrics
+
 list1 = ["Sorry", "What do you mean", "Same Old Love", "Here", "Hit The Quan", "Good For You"]
 list2 = ["Focus", "Where Ya At", "Hello", "Locked Away", "Lean On", "Confident"]
 list3 = ["Jumpman", "Trap Queen", "Renegades", "White Iverson", "Shut Up and Dance", "Hotline Bling"]
@@ -10,23 +13,27 @@ list5 = ["My Way", "Ex's & Oh's", "Antidote", "Like I'm gonna lose you", "Tennes
 indicoio.config.api_key = 'a894b47f344116b3d32d12de039bf690'
 text_input = input('Tell me about your day: ')
 sentiment = indicoio.sentiment_hq(text_input)
+songTitle = ""
+dayDescription = ""
 print(sentiment)
 #0<SUPERSAD<.25<SAD<.45<NEUTRAL<.6<HAPPY<.8<SUPERHAPPY<1
 if sentiment < .15:
-    print("Your day was: AWFUL")
-    return(list1[(int)(random.random()*len(list1))])
+    dayDescription = ("Your day was: AWFUL")
+    songTitle = (list1[(int)(random.random()*len(list1))])
 else:
     if sentiment < .35:
-        print("Your day was: BAD")
-        return(list2[(int)(random.random()*len(list2))])
+        dayDescription = ("Your day was: BAD")
+        songTitle = (list2[(int)(random.random()*len(list2))])
     else:
         if sentiment < .65:
-            print("Your day was: OK")
-            return(list3[(int)(random.random()*len(list3))])
+            dayDescription = ("Your day was: OK")
+            songTitle = (list3[(int)(random.random()*len(list3))])
         else:
             if sentiment <.9:
-                print("Your day was: GOOD")
-                return(list4[(int)(random.random()*len(list4))])
+                dayDescription = ("Your day was: GOOD")
+                songTitle = (list4[(int)(random.random()*len(list4))])
             else:
-                print("Your day was: AWESOME")
-                return(list5[(int)(random.random()*len(list5))])
+                dayDescription = ("Your day was: AWESOME")
+                songTitle = (list5[(int)(random.random()*len(list5))])
+print(dayDescription)
+print(songTitle)
