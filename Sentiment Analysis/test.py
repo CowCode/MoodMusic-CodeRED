d05ef78f4613a31ec9da667a93b58b3f46b2ac8e
j-yang <joshua.yang16@gmail.com>
2015-11-22 07:01:31 -0600

    redo

--- Sentiment Analysis/test.py
+++ Sentiment Analysis/test.py
@@ -2,4 +2,5 @@ import indicoio
 indicoio.config.api_key = 'a894b47f344116b3d32d12de039bf690'
 text_input = input('Tell me about your day: ')
 print(indicoio.sentiment_hq(text_input))
-#0<SUPERSAD<.25<SAD<.4<NEUTRAL<.6<
+#0<SUPERSAD<.25<SAD<.45<NEUTRAL<.6<HAPPY<.8<SUPERHAPPY<1
+if 
