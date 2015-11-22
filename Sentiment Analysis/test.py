0db7ddb41c976af6f71de234ab84719423706235
j-yang <joshua.yang16@gmail.com>
2015-11-22 07:12:28 -0600

    Specific ranges defined through tests

--- Sentiment Analysis/test.py
+++ Sentiment Analysis/test.py
@@ -1,6 +1,19 @@
 import indicoio
 indicoio.config.api_key = 'a894b47f344116b3d32d12de039bf690'
 text_input = input('Tell me about your day: ')
-print(indicoio.sentiment_hq(text_input))
+sentiment = indicoio.sentiment_hq(text_input)
+print(sentiment)
 #0<SUPERSAD<.25<SAD<.45<NEUTRAL<.6<HAPPY<.8<SUPERHAPPY<1
-if 
+if sentiment < .15:
+    print ("SUPERSAD")
+else:
+    if sentiment < .40:
+        print ("SAD")
+    else:
+        if sentiment < .65:
+            print ("NEUTRAL")
+        else:
+            if sentiment <.9:
+                print ("HAPPY")
+            else:
+                print ("SUPERHAPPY")
