d3cf6e08dcc82d28ea051f488d04665818a0a777
talpallikar <arjuntalpallikar@utexas.edu>
2015-11-22 06:58:52 -0600

    chris pulicken is a god

--- PracticeWithDjango/Container/mysite/urls.py
+++ PracticeWithDjango/Container/mysite/urls.py
@@ -17,8 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
-  ##  url(r'^index/', include('index.urls')),
+    url(r'^polls/', include('polls.urls')),
+    url(r'^$', include('polls.urls')),
     ##url(r'^result/', include('results.urls')),
 ]
--- PracticeWithDjango/Container/polls/urls.py
+++ PracticeWithDjango/Container/polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.results, name='results'),
+    url(r'^index$', views.index, name='index'),
+    url(r'^results$', views.results, name='results'),
 ]
